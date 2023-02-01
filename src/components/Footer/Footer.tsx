4e8abee5b38b18b3b998eeb55fcf5c2faa1f53bf
berkutoff1994 <79610401049@yandex.ru>
2023-02-01 17:22:23 +0500

    component footer has been changed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import styles from './footer.css';
+import styles from './footer.module.scss';
 
 export function Footer() {
   return (
-
+    <footer>
+      
+    </footer>
   );
 }
