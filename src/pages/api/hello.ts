012e09e153301697c54f6538c07dc2ba825429f0
berkutoff1994 <79610401049@yandex.ru>
2023-02-02 11:33:01 +0500

    api functions have been changed

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from "axios";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export const fetcher = (url: string, init?: RequestInit) => axios.post(url).then(res => res.data.JSON())
