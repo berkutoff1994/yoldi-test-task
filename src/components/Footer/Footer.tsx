f83a1c4e79f133fde0f99f2449e179a0d564e41c
berkutoff1994 <79610401049@yandex.ru>
2023-02-02 11:28:32 +0500

    footer has been changed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { FC } from 'react';
 import styles from './footer.module.scss';
 
-export function Footer() {
+export const Footer = () => {
+  const router = useRouter()
+
   return (
-    <footer>
-      
+    <footer className={styles.footer}>
+      <div className={styles.footer__content}>
+        {router.pathname.includes('register')
+          ?
+          <div className={styles.footer__contentBlock}>
+            <span className={styles.footer__question}>
+              Уже есть аккаунт?
+            </span>
+            <Link href='/login' className={styles.footer__link}> Войти</Link>
+          </div>
+          :
+          <div className={styles.footer__contentBlock}>
+            <span className={styles.footer__question}>
+              Еще нет аккаунта?
+            </span>
+            <Link href='/register' className={styles.footer__link}> Зарегистрироваться</Link>
+          </div>
+        }
+      </div>
     </footer>
   );
 }
