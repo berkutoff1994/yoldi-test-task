3015388e39e33503a8b0abf4bf7835bc918302d7
berkutoff1994 <79610401049@yandex.ru>
2023-02-03 20:40:50 +0500

    heading has been changed

--- src/components/header/Heading/Heading.tsx
+++ src/components/header/Heading/Heading.tsx
@@ -1,24 +1,47 @@
 import { FC } from 'react';
 import Image from 'next/image';
-import styles from './heading.module.scss';
 import EnterButton from '@/components/ui/EnterButton/EnterButton';
+import Link from 'next/link';
+import { IUser } from '@/types';
+import styles from './heading.module.scss';
 
-interface IHeadingProps {
-  user: boolean,
+interface IHeading {
+  myUser?: IUser | undefined
 }
 
-export const Heading:FC<IHeadingProps> = ({user}) => {
+export const Heading:FC<IHeading> = ({myUser}) => {
   return (
     <div className={styles.heading}>
       <div className={styles.container}>
         <div className={styles.heading__content}>
           <div className={styles.heading__logoBlock}>
-            <Image alt='logo' src='/logo.png' width={80} height={50}></Image>
+            <Link href={'/accounts'}>
+              <Image alt='logo' src='/logo.png' width={80} height={50} />
+            </Link>
             <h2 className={styles.heading__title}>
               Разрабатываем и запускаем сложные веб проекты
             </h2>
           </div>
-          <EnterButton>Войти</EnterButton>
+          {myUser 
+            ? 
+            <div className={styles.heading__avatarBlock}>
+              <span className={styles.avatarBlock__name}>
+                {myUser.name}
+              </span>
+              <Link href={`/account/owner/${myUser.email}`}>
+                <div className={styles.avatarBlock__avatar}>
+                  {myUser.image
+                  ?
+                  <Image alt='avatar' src={myUser.image.url} width={50} height={50}/>
+                  :
+                  <div className={styles.avatar}>{myUser.name[0]}</div>
+                  }
+                </div>
+              </Link>
+            </div>
+            :
+            <Link href='/login'><EnterButton>Войти</EnterButton></Link>
+          }
         </div>
       </div>
     </div>
