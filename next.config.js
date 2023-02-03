2c05094c321ce7e3a125d0a9d71c0f85b828e31a
berkutoff1994 <79610401049@yandex.ru>
2023-02-03 20:37:51 +0500

    config has been changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'frontend-test-api.yoldi.agency',
+        port: '',
+        pathname: '/api/image/src/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
