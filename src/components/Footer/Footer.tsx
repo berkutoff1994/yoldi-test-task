7fc694348cc3907dadc67cf959a6671a81be7096
berkutoff1994 <79610401049@yandex.ru>
2023-02-03 20:40:34 +0500

    foooter has been changed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { FC } from 'react';
 import styles from './footer.module.scss';
 
 export const Footer = () => {
   const router = useRouter()
-
   return (
     <footer className={styles.footer}>
       <div className={styles.footer__content}>
